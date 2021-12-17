65683d38ea67b0f6808b6101d40be95273b154b9
Daniel Sonobe Silveira <dev.obe78@gmail.com>
2021-12-17 12:17:17 -0300

    alguns argumentos sobre o método

--- metodo_concat.js
+++ metodo_concat.js
@@ -3,13 +3,16 @@ let item = 3;
 
 console.log([1, 2].concat(item))
 
+/* Como o método .concat funciona tanto em valores como em Arrays, também podemos implementar uma 
+função que se comporta como o método .flat facilmente: */
 
-function flat (array, nesteArray){
+function flat(array, nesteArray){
 
     return array.concat(...nesteArray)
 
 }
 
 flat([1, 2], [3, 4, [5, 6]]);
-
 console.log(flat([1, 2], [3, 4, [5, 6]]));
+
+/* O operador spread usa cada um dos elementos nesteArray como argumentos para a chamada do método .concat */
