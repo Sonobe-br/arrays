b2b0c63331c0f8ccf7c19af3d88ed9f2bf5d7417
Daniel Sonobe Silveira <dev.obe78@gmail.com>
2021-12-19 11:26:50 -0300

    Arrays-js: trabalhando com o método indexOf para atualizar a lista de peças automotiva de no sistema de uma empresa de Autopeças

---
+++ metodo_indexOf.js
@@ -0,0 +1,21 @@
+let pecas = ['bomba dágua', ' tensores', ' correia dentada', ' pastilhas', ' sapatas', ' bobinas', ' cabos de vela'];
+
+function atualizarColecaoAutoPecas(pecas, peca){ //pecas & peca se refere a palavra 'peças'.
+
+    if (pecas.indexOf(peca) === -1) {
+
+        pecas.push(peca);
+        
+        console.log('Um novo produto foi adiconado na lista, verifique a atualização: ' + pecas);
+
+    } else if (pecas.indexOf(peca) > -1) {
+
+        console.log(`${peca}, é um item que já existe na coleção de peças`)
+
+    }  
+    
+}
+
+atualizarColecaoAutoPecas(pecas, ' Sinto de segurança');
+
+console.log(atualizarColecaoAutoPecas);
